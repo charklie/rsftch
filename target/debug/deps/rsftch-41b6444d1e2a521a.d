3b17c70a1292a391ea8938ef25cee0f0789f899e
charklie <charliejohanid@gmail.com>
2024-03-23 16:30:17 +0100

    add

--- target/debug/deps/rsftch-41b6444d1e2a521a.d
+++
@@ -1,7 +0,0 @@
-/home/chararch/Documents/Programming/Rust/rsftch/target/debug/deps/rsftch-41b6444d1e2a521a: src/main.rs
-
-/home/chararch/Documents/Programming/Rust/rsftch/target/debug/deps/rsftch-41b6444d1e2a521a.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:XDG_CURRENT_DESKTOP=Hyprland

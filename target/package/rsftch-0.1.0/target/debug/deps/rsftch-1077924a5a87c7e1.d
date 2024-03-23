e585fe7e388c8f8b1455c497927a4abb522727b0
charklie <charliejohanid@gmail.com>
2024-03-23 15:37:14 +0100

    add

---
+++ target/package/rsftch-0.1.0/target/debug/deps/rsftch-1077924a5a87c7e1.d
@@ -0,0 +1,7 @@
+/home/chararch/rsftch/target/package/rsftch-0.1.0/target/debug/deps/rsftch-1077924a5a87c7e1: src/main.rs
+
+/home/chararch/rsftch/target/package/rsftch-0.1.0/target/debug/deps/rsftch-1077924a5a87c7e1.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:XDG_CURRENT_DESKTOP=Hyprland
---
+++ target/package/rsftch-0.1.0/target/release/rsftch.d
@@ -0,0 +1,1 @@
+/home/chararch/rsftch/target/release/rsftch: /home/chararch/rsftch/src/main.rs

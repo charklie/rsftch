61791c49837b8f4f34d1285d357ca78bda9fa050
charklie <charliejohanid@gmail.com>
2024-03-23 16:36:27 +0100

    add

---
+++ target/package/rsftch-0.1.4/target/debug/rsftch.d
@@ -0,0 +1,1 @@
+/home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.4/target/debug/rsftch: /home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.4/src/main.rs

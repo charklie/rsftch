99abb1ef8508e7fc2995a037dd8003a258ebd591
charklie <charliejohanid@gmail.com>
2024-03-24 11:56:51 +0100

    final push before publish 0.1.8

---
+++ target/package/rsftch-0.1.7/target/debug/deps/rsftch-67795fd2f9e5cbc4.d
@@ -0,0 +1,7 @@
+/home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.7/target/debug/deps/rsftch-67795fd2f9e5cbc4: src/main.rs
+
+/home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.7/target/debug/deps/rsftch-67795fd2f9e5cbc4.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:XDG_CURRENT_DESKTOP=Hyprland
---
+++ target/package/rsftch-0.1.7/target/debug/rsftch.d
@@ -0,0 +1,1 @@
+/home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.7/target/debug/rsftch: /home/chararch/Documents/Programming/Rust/rsftch/target/package/rsftch-0.1.7/src/main.rs
